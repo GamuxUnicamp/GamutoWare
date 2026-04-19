93eb064317129b218170ad0623fc68bf0bd2d0ad
William Dos Santos Junqueira <ra306568@kiss.lab.ic.unicamp.br>
2026-04-19 18:29:07 -0300

    update game

---
+++ microjogos/2026S1/projetowilliam/cenas/character_body_2d.gd
@@ -0,0 +1,44 @@
+extends CharacterBody2D
+
+
+const SPEED = 450.0
+const gravity = 50
+const fast_fall = 300
+
+
+"""
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump. 
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
+"""
+
+func _physics_process(delta: float) -> void:
+
+	if Input.is_action_pressed("ui_accept"):
+		velocity.y += fast_fall * delta
+
+	else: velocity.y += gravity * delta
+	
+	var direction := Input.get_axis("esquerda", "direita")
+	if direction: velocity.x = direction * SPEED
+	else: velocity.x = move_toward(velocity.x, 0, SPEED)
+	var colision = move_and_collide(velocity*delta)
+	if colision:
+		Minigames.register_lose(self)
+		
+	
