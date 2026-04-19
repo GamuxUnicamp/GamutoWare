65080d0aafcbd3805d0502b8f2fa838e8a4249d4
Lucas de Queiroz Menezes Herédia <lucas.heredia@students.ic.unicamp.br>
2026-04-18 21:56:32 -0300

    Física e detecção de vitória concluídos

--- microjogos/2026S1/projeto-lucas/cenas/scripts/nave.gd
+++ microjogos/2026S1/projeto-lucas/cenas/scripts/nave.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 const MAX_VELOCITY = 1000
-const ACCELERATION = 200
+const PROPULSION = 200
 const ANGULAR_VELOCITY = PI/1.5
 
 var lua
@@ -9,12 +9,25 @@ func _ready() -> void:
 	lua = get_node("../Lua")
 
 func _physics_process(delta: float) -> void:
-	# Velocidade varia conforme aceleração
+	# Define aceleração = propulsão + gravidade
+	var acceleration: Vector2 = gravityAcceleration()
 	if Input.is_action_pressed("acao") and velocity.length() != MAX_VELOCITY:
-		velocity += Vector2.UP.rotated(rotation)*ACCELERATION*delta
+		acceleration += Vector2.UP.rotated(rotation)*PROPULSION
+	
+	velocity += acceleration*delta
 
 	# Rotação varia com setinhas
-	var angleDirection := Input.get_axis("esquerda", "direita")
+	var angleDirection = Input.get_axis("esquerda", "direita")
 	rotation += angleDirection*ANGULAR_VELOCITY*delta
 
-	move_and_slide()
+	if move_and_collide(velocity*delta) and velocity.length()<250:
+		Minigames.register_win(self)
+		print(velocity.length())
+
+func gravityAcceleration():
+	var directionVec: Vector2 = lua.position-position
+	var normalizedVec: Vector2 = directionVec.normalized()
+	
+	var acceleration = (10**6.7)*normalizedVec/(directionVec.length_squared()-6767)
+	
+	return acceleration
