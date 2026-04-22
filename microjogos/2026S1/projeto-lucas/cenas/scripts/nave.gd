42a1d29db02f7fe97cdfb9feebca5be237992ffa
luqhe <90876462+luqhe@users.noreply.github.com>
2026-04-21 23:06:15 -0300

    Música e efeitos sonoros adicionados

--- microjogos/2026S1/projeto-lucas/cenas/scripts/nave.gd
+++ microjogos/2026S1/projeto-lucas/cenas/scripts/nave.gd
@@ -4,8 +4,10 @@ const PROPULSION = 200
 const ANGULAR_VELOCITY = PI/1.5
 
 var lua
+var bandeira
 func _ready() -> void:
 	lua = get_node("../Lua")
+	bandeira = get_node("../Bandeira")
 	
 	# Áudio da nave
 	$AudioPropulsao.play()
@@ -13,7 +15,7 @@ func _ready() -> void:
 	
 	# Define distância ao centro da lua e variação em relação ao eixo x
 	var initialPosition = randfn(600, 50)
-	var initialPositionAngle = randf_range(-PI/4, PI/4)
+	var initialPositionAngle = randf_range(-PI/5, PI/5)
 	# Define lado em que nave spawna (side=0 -> direita, side=1 -> esquerda)
 	var side = randi()%2
 	var displacement = Vector2.RIGHT.rotated(initialPositionAngle+(PI*side))*initialPosition
@@ -25,8 +27,28 @@ func _ready() -> void:
 	velocity = Vector2.UP.rotated(initialVelocityAngle)*initialVelocity
 
 
-var win: bool = false
+var end: bool = false
 func _physics_process(delta: float) -> void:
+	if not end:
+		var acceleration = find_acceleration()
+		velocity += acceleration*delta
+		
+		# Rotação varia com setinhas
+		var angleDirection = Input.get_axis("esquerda", "direita")
+		rotation += angleDirection*ANGULAR_VELOCITY*delta
+
+	if move_and_collide(velocity*delta) and not end:
+		end = true
+		$AudioPropulsao.stream_paused = true
+		$Trail.emitting = false
+		
+		if velocity.length()<225:
+			Minigames.register_win(self)
+			win_explosion()
+		else: lose_explosion()
+
+
+func find_acceleration() -> Vector2:
 	# Define aceleração = propulsão + gravidade
 	var acceleration: Vector2 = gravityAcceleration()
 	if Input.is_action_pressed("acao"):
@@ -35,25 +57,17 @@ func _physics_process(delta: float) -> void:
 		# Animação e som
 		$Sprite2D/AnimationPlayer.play("acelerado")
 		$AudioPropulsao.stream_paused = false
+		
+		$Trail.gravity = Vector2.UP.rotated(-rotation)
+		$Trail.emitting = true
 	else:
 		# Animação e som
 		$Sprite2D/AnimationPlayer.play("parado")
 		$AudioPropulsao.stream_paused = true
-	
-	velocity += acceleration*delta
-
-	# Rotação varia com setinhas
-	var angleDirection = Input.get_axis("esquerda", "direita")
-	rotation += angleDirection*ANGULAR_VELOCITY*delta
-
-	if move_and_collide(velocity*delta) and not win:
-		if velocity.length()<225:
-			Minigames.register_win(self)
-			win = true
-			
-			win_explosion()
-		else: lose_explosion()
+		$Trail.emitting = false
 		
+	return acceleration
+
 
 func gravityAcceleration() -> Vector2:
 	var directionVec: Vector2 = lua.position-position
@@ -67,13 +81,28 @@ func gravityAcceleration() -> Vector2:
 func lose_explosion() -> void:
 	$Sprite2D.visible = false
 	
+	# Explosion particles
 	$ExplosionParticles.rotation = -rotation
 	$ExplosionParticles.direction = velocity.normalized()
 	$ExplosionParticles.emitting = true
+	
+	# Explosion animation
+	$ExplosionSprite.rotation = velocity.angle()-rotation+PI/2
+	$ExplosionSprite.visible = true
+	$ExplosionSprite/AnimationPlayer.play("explodir")
 	
 	$AudioExplosao.play()
 
 
 func win_explosion() -> void:
+	# Ajusta a posição da nave
 	var luaNave: Vector2 = position-lua.position
 	rotation = luaNave.angle()+PI/2
+	
+	# Bandeira
+	var posBandeira = lua.position+luaNave.rotated(PI/11)
+	bandeira.position = posBandeira
+	bandeira.rotation = (posBandeira-lua.position).angle()+PI/2
+	bandeira.get_node("AnimationPlayer").play("balangar")
+	
+	$Sprite2D/AnimationPlayer.play("win_rotation")
